b0689bce4ebdf2e0e741c974fd58b16b8c8daa1a
Anand8991 <ee21mtech14022@iith.ac.in>
2022-01-17 12:39:07 +0530

    repo

--- assignment 1/booleanc.c
+++ assignment 1/booleanc.c
@@ -5,8 +5,9 @@ int main(){
     unsigned char one = 0x01;//used for displaying the output in bit
     unsigned char  X=0x00,Y=0x01,Z=0x01,W=0x00; //inputs in hex
 
-    //F = (Z & (~W))|(Y & (Z))|((X) & (~Y)) | (X & W);
-    F=~((~(Z&~(W)))&(~(Y&Z))&(~(X&~(Y)))&(~(X&(W))));
+    //F = (Z & (~W))|(Y & (Z))|((X) & (~Y)) | (X & W);//SOP implementation using AND-OR logic
+
+    F=~((~(Z&~(W)))&(~(Y&Z))&(~(X&~(Y)))&(~(X&(W))));//SOP implementation using NAND logic
 
     printf("%x%x%x%x",one&X,one&Y,one&Z,one&W);
     printf(" ");
